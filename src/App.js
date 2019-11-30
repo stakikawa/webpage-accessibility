4023b15b76ac7dde661f52cc5ed589ea21f8d880
Apricot_HT <haotiangong@hotmail.com>
2019-11-30 15:31:16 -0800

    Update App.js

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import './App.css';
 import Homepage from './components/homepage.jsx';
 import Header from './components/header.jsx';
 import AltPicture from './components/altpicture.jsx';
+import Text from './components/text.jsx';
 
 function App() {
   return (
     <div className="App">
       <Homepage />
-      <Header text="This is a header!" />
+          <Header text="This is a header!" />
+          <Text text= "This is some text." />
       <AltPicture source="https://www.petmd.com/sites/default/files/senior-golden-retriever-with-ball-picture-id488657289.jpg" newAlt="This is a picture of a dog" />
     </div>
   );
