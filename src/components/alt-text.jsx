294d7c940f38a4fac2fffe06ce68d24e74a04b96
stakikawa <suzuran.takikawa@gmail.com>
2019-11-30 17:00:05 -0800

    Add azure alt-text component

---
+++ src/components/alt-text.jsx
@@ -0,0 +1,32 @@
+require('dotenv').config();
+const async = require('async');
+const ComputerVisionClient = require('@azure/cognitiveservices-computervision').ComputerVisionClient;
+const ApiKeyCredentials = require('@azure/ms-rest-js').ApiKeyCredentials;
+
+function computerVision() {
+    async.series([
+      async function () {},
+      function () {
+        return new Promise((resolve) => {
+          resolve();
+        })
+      }
+    ], (err) => {
+      throw (err);
+    });
+  }
+  
+  computerVision();
+
+
+let key = process.env.COMPUTER_VISION_SUBSCRIPTION_KEY;
+let endpoint = process.env.COMPUTER_VISION_ENDPOINT;
+if (!key) { throw new Error('Set your environment variables for your subscription key and endpoint.'); }
+
+let computerVisionClient = new ComputerVisionClient(new ApiKeyCredentials({inHeader: {'Ocp-Apim-Subscription-Key': key}}), endpoint);
+
+module.exports.getDescription = 
+async function getDescription(describeURL) {
+  let caption = await computerVisionClient.describeImage(describeURL);
+  return caption.captions[0].text;
+};
